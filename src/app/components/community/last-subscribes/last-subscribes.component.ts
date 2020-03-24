2fada92bb79c3a3b11cd3a7638f789d0811dfb6b
vincent78-devweb <55990401+vincent78-devweb@users.noreply.github.com>
2020-03-24 15:14:31 +0100

    ajout derniers inscrits

--- src/app/components/community/last-subscribes/last-subscribes.component.ts
+++ src/app/components/community/last-subscribes/last-subscribes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UsersListPager } from '../../../models/community/users-list-pager';
+import { PagerParams } from '../../../models/commons/pager-params';
 import { UsersService } from '../../../services/users/users.service';
 import { User } from '../../../models/community/user';
 
@@ -10,14 +12,74 @@ import { User } from '../../../models/community/user';
 })
 export class LastSubscribesComponent implements OnInit {
 
-  lastSubscribes: User[] = [];
+  // Breadcrumb parameters
+  title: string = "La communauté";
+  level1: string = "Derniers inscrits";
+  level2: string = "";
 
-  constructor( private usersService: UsersService ) { }
+  isListVisible = true;
+  isUserVisible = false;
 
-  ngOnInit() {
-    this.usersService.getLastSubscribes().subscribe(data => {
-      this.lastSubscribes = data;
-    })
+  pager: PagerParams;
+
+  users: User[] = [];
+  user: User;
+
+  constructor( private usersService: UsersService ) { 
+    this.pager = {
+      currentPage: 0,
+      totalPages: 0,
+      pageTotalElements: 0,
+      totalElements: 0,
+      size: 0
+    };
   }
 
+  ngOnInit() {
+    this.paginate(0);
+  }
+
+  onPaginate(pageNumber){
+    this.paginate(pageNumber);
+  }
+
+  /**
+   * Paginate to the sent page
+   * 
+   * @param filters 
+   * @param pageNumber
+   * @see https://jasonwatmore.com/post/2019/06/18/angular-8-simple-pagination-example 
+   */
+  paginate(pageNumber: number) {
+    this.usersService.filterUsers('', '', '', 0, 0, pageNumber, 20, "dateCreation,desc").subscribe(data => {
+      const userListpager: UsersListPager = data;
+      this.users = data.content;
+
+      let currentPager: PagerParams;
+      currentPager = {
+        currentPage : userListpager.number,
+        totalPages : userListpager.totalPages,
+        pageTotalElements : this.users.length,
+        totalElements : userListpager.totalElements,
+        size : userListpager.size
+      }
+
+      this.pager = currentPager;
+
+    });
+  }
+
+  showUser(user: User) {
+    this.user = user;
+    this.level2 = user.firstname + " " + user.lastname;
+    this.isListVisible = false;
+    this.isUserVisible = true;
+  }
+
+  notifyHideUSer($event) {
+    this.user = undefined;
+    this.level2 = '';
+    this.isListVisible = true;
+    this.isUserVisible = false;
+  }
 }
